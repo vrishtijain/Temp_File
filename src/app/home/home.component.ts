5fa0139f873730425f86867e23d58bfa866b87b6
Vrishti Jain <vrishti.jain16@gmail.com>
2020-05-23 23:54:06 -0400

    puch

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { HomeService } from './../home.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public a;
+  constructor(private _valid: HomeService) { }git 
+
+  ngOnInit(): void {
+  }
+  public upload(event: any) {
+    let files = event.target.files;
+    //check file is valid
+    if (!this._valid.validateFile(files[0].name)) {
+      console.log('Selected file format is not supported');
+
+      window.alert("Format nor supported")
+      return false;
+    }
+    else{
+       console.log(files);
+    if (files && files.length > 0) {
+      let file: File = files.item(0);
+      console.log(file.name);
+      console.log(file.size);
+      console.log(file.type);
+      let reader: FileReader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (e) => {
+        let csv: string = reader.result as string;
+        console.log(csv);
+        this.a = csv;
+    }
+
+  }}}
+
+
+  // public changeListener(files: FileList) {
+   
+
+  //     }
+  //   }
+  // }
+
+
+
+
+}
